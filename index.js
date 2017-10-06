8acc8f010b3d4ac9abf7c6714d642ae8f8bfe1da
garthdb <garthdb@gmail.com>
2017-10-06 10:51:51 -0600

    initial commit, with @mijordan's polyfill

---
+++ index.js
@@ -0,0 +1,61 @@
+// Provides explicit indication of elements receiving focus through keyboard interaction rather than mouse or touch.
+(function(document) {
+  var NAVIGATION_KEYS = [
+    'Tab',
+    'ArrowUp',
+    'ArrowRight',
+    'ArrowDown',
+    'ArrowLeft',
+    'Home',
+    'End',
+    'PageUp',
+    'PageDown',
+    'Enter',
+    ' ',
+    'Escape',
+
+    /* IE9 and Firefox < 37 */
+    'Up',
+    'Right',
+    'Down',
+    'Left',
+    'Esc'
+  ];
+  var keyboardFocus = false;
+  var elements = document.querySelectorAll('.focus-ring');
+
+  function onKeydownHandler(event) {
+    if (event.ctrlKey || event.altKey || event.metaKey || NAVIGATION_KEYS.indexOf(event.key) === -1) {
+      return;
+    }
+    keyboardFocus = true;
+
+    if (document.activeElement && document.activeElement !== document.body) {
+      document.activeElement.classList.add('focus-ring');
+    }
+  }
+
+  function onMousedownHandler() {
+    keyboardFocus = false;
+
+    for (var i = 0; i < elements.length; i++) {
+      elements[i].classList.remove('focus-ring');
+    }
+
+  }
+
+  function onFocusHandler(event) {
+    if (keyboardFocus) {
+      event.target.classList.add('focus-ring');
+    }
+  }
+
+  function onBlurHandler(event) {
+    event.target.classList.remove('focus-ring');
+  }
+
+  document.addEventListener('keydown', onKeydownHandler, true);
+  document.addEventListener('mousedown', onMousedownHandler, true);
+  document.addEventListener('focus', onFocusHandler, true);
+  document.addEventListener('blur', onBlurHandler, true);
+}(document));
